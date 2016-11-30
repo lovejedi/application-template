39e859a3a70bcfe48a2a972dbaa32154af5d8356
raydecastro <captray108@gmail.com>
2016-11-29 17:00:11 -0800

    feature. updated script.

--- resetgit.sh
+++ resetgit.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
-echo ".love> wiping out git repo history"
+echo "\n.love> wiping out git repo history"
 
 rm -rf .git
 
 
-echo ".love> git init"
+echo "\n.love> git init"
 
 git init
 
-echo ".love> happy coding. :)"
+echo "\n.love> git add ."
+
+git add .
+
+echo "\n.love> git commit -m \"feature. added node.js + es5 project scaffold to source control.\""
+
+git commit -m "feature. added node.js + es5 project scaffold to source control."
+
+echo "\n.love> locked and loaded."
+
+echo "\n.love> happy coding. :)"
