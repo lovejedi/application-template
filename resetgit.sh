7584d2055dddf1d571e735884813f101abf7c890
raydecastro <captray108@gmail.com>
2016-11-29 17:15:20 -0800

    feature. minor tweaks on script. ;)

--- resetgit.sh
+++ resetgit.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-printf "\n\n.love> is the most powerful force in the universe."
+printf "\n.love> is the most powerful force in the universe."
 
-printf "\n\n.love> # wiping out git repo history"
+printf "\n\n.love> wiping out git repo history"
+
 printf "\n\n.love> rm -rf .git"
 
 rm -rf .git
@@ -11,7 +12,7 @@ printf "\n\n.love> git init\n\n"
 
 git init
 
-printf "\n\n.love> git add ."
+printf "\n.love> git add ."
 
 git add .
 
@@ -19,6 +20,6 @@ printf "\n.love> git commit -m \"feature. added node.js + es5 project scaffold t
 
 git commit -m "feature. added node.js + es5 project scaffold to source control."
 
-printf "\n\n.love> locked and loaded."
+printf "\n.love> locked and loaded."
 
 printf "\n\n.love> happy coding. :)\n\n"
